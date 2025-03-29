248f30409d8db8e54585e41fea76be1a1e0a98ad
Fredrick M. Morara <160414428+fredymorara@users.noreply.github.com>
2025-03-29 11:27:54 +0300

    new file:   .env 	modified:   src/services/api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:5000'; // Replace with your backend URL
+export const API_URL = process.env.REACT_APP_API_URL || 'http:localhost:5000'
 
 // Register a new user
 export const register = async (userData) => {
